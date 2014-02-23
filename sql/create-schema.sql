c708ed8a6ebb046fca1ecc6b4fcec6edd4c956cf
eras0r <er4s0r@gmail.com>
2014-02-23 04:03:58 +0100

    added create-schema.sql script

---
+++ sql/create-schema.sql
@@ -0,0 +1,29 @@
+-- --------------------------------------------------------
+-- This file generates the required schema for the
+-- for the borderlands item database backend application.
+-- --------------------------------------------------------
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `rarity`
+--
+CREATE TABLE IF NOT EXISTS `rarity` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `color` varchar(7) COLLATE utf8_unicode_ci NOT NULL,
+  `sortOrder` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_name` (`name`),
+  UNIQUE KEY `unique_sortOrder` (`sortOrder`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `manufacturer`
+--
+CREATE TABLE IF NOT EXISTS `manufacturer` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
