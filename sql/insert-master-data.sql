b089335ab7a5cfdbfe551bd4907ffe1b0ff867e0
eras0r <er4s0r@gmail.com>
2014-02-23 04:05:17 +0100

    added insert-master-data.sql script

---
+++ sql/insert-master-data.sql
@@ -0,0 +1,17 @@
+-- --------------------------------------------------------
+-- This file adds required master data
+-- for the borderlands item database backend application.
+-- --------------------------------------------------------
+
+--
+-- Dumping data for table `rarity`
+--
+INSERT INTO `rarity` (`id`, `name`, `color`, `sortOrder`) VALUES
+(1, 'common', '#ffffff', 1),
+(2, 'green', '#00ff00', 2),
+(3, 'blue', '#2f78ff', 3),
+(4, 'purple', '#9132c8', 4),
+(5, 'e-tech', '#ff00ff', 5),
+(6, 'legendary', '#ff9600', 6),
+(7, 'pearlescent', '#00ffff', 7),
+(8, 'seraph', '#ff69b4', 8);
