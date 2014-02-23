012bb90b1e6858d3176c12e6ae1c7236578c6998
eras0r <er4s0r@gmail.com>
2014-02-23 04:50:00 +0100

    addde manufacturer master data to insert-master-data.sql script

--- sql/insert-master-data.sql
+++ sql/insert-master-data.sql
@@ -6,12 +6,28 @@
 --
 -- Dumping data for table `rarity`
 --
-INSERT INTO `rarity` (`id`, `name`, `color`, `sortOrder`) VALUES
-(1, 'common', '#ffffff', 1),
-(2, 'green', '#00ff00', 2),
-(3, 'blue', '#2f78ff', 3),
-(4, 'purple', '#9132c8', 4),
-(5, 'e-tech', '#ff00ff', 5),
-(6, 'legendary', '#ff9600', 6),
-(7, 'pearlescent', '#00ffff', 7),
-(8, 'seraph', '#ff69b4', 8);
+INSERT INTO `rarity` (`name`, `color`, `sortOrder`) VALUES
+('common', '#ffffff', 1),
+('green', '#00ff00', 2),
+('blue', '#2f78ff', 3),
+('purple', '#9132c8', 4),
+('e-tech', '#ff00ff', 5),
+('legendary', '#ff9600', 6),
+('pearlescent', '#00ffff', 7),
+('seraph', '#ff69b4', 8);
+
+--
+-- Dumping data for table `manufacturer`
+--
+INSERT INTO `manufacturer` (`name`) VALUES
+('Anshin'),
+('Bandit'),
+('Dahl'),
+( 'Eridian'),
+('Hyperion'),
+('Jakobs'),
+('Maliwan'),
+( 'Pangolin'),
+('Tediore'),
+('Torgue'),
+('Vladof');
